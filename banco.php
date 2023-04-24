e7d203d0d0c4d4162f9c639b2b47d01f05af30d4
NomeJaEmUso <andreyvanolli@gmail.com>
2023-04-24 17:49:54 -0300

    adição banco de dados

--- banco.php
+++ banco.php
@@ -10,23 +10,6 @@ $conn = new mysqli($localhost, $root, $root, $UrbanClub);
 
 // Verificando se a conexão foi estabelecida com sucesso
 if ($conn->connect_error) {
-    die("Falha na conexão: " . $conn->connect_error);
+  die("Falha na conexão: " . $conn->connect_error);
 }
-
-// Executando a consulta
-$sql = "SELECT * FROM cadastro";
-$result = $conn->query($sql);
-
-// Exibindo os resultados
-if ($result->num_rows > 0) {
-    // Saída dos dados de cada linha
-    while($row = $result->fetch_assoc()) {
-        echo "Nome: " . $row["nome"]. " - Email: " . $row["email"]. "<br>";
-    }
-} else {
-    echo "Nenhum resultado encontrado.";
-}
-
-// Fechando a conexão
-$conn->close();
-?>
+echo "Conexão bem sucedida";
