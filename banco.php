91421ac028a26ea33a81478e973817432dc38af1
NomeJaEmUso <andreyvanolli@gmail.com>
2023-04-28 15:29:39 -0300

    banco de dados

--- banco.php
+++ banco.php
@@ -13,9 +13,3 @@ if ($conn->connect_error) {
   die("Falha na conexão: " . $conn->connect_error);
 }
 echo "Conexão bem sucedida";
-
-
-$nm_username= $_POST['apelido'];
-$cd_email = $_POST['email'];
-$cd_password = $_POST['senha'];
-$cd_corfim_password = $_POST['confirmar-senha'];
---
+++ cadastro.php
@@ -0,0 +1,8 @@
+<?php
+
+$nm_username= $_POST['apelido'];
+$cd_email = $_POST['email'];
+$cd_password = $_POST['senha'];
+$cd_corfim_password = $_POST['confirmar-senha'];
+echo "Cadastro realizado com sucesso!";
+?>
