77edccd5ce1a0a7f34a9b4e01c9c8392fa3ec318
NomeJaEmUso <andreyvanolli@gmail.com>
2023-04-28 17:05:42 -0300

    Tô surtando

--- cadastro.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$nm_username= $_POST['apelido'];
-$cd_email = $_POST['email'];
-$cd_password = $_POST['senha'];
-$cd_corfim_password = $_POST['confirmar-senha'];
-echo "Cadastro realizado com sucesso!";
-?>
