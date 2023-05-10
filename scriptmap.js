9fe567c106ccaa02fb43d019973d8506cacca4bd
vonmacedo <mjoaovictor388@gmail.com>
2023-05-10 08:19:06 -0300

    Adição barra de pesquisa - meio funcional

--- scriptmap.js
+++ scriptmap.js
@@ -33,12 +33,54 @@ var map;
 				type: "basquete"
 			},
 
+            
 		];
-		function initMap() {
-			map = new google.maps.Map(document.getElementById('map'), {
-				center: {lat: -23.9608, lng:-46.3331},
-				zoom: 14
-			});
+        function initMap() {
+            const bounds = new google.maps.LatLngBounds(
+              new google.maps.LatLng(-23.98604, -46.396624), // South west of Santos
+              new google.maps.LatLng(-23.928531, -46.266352), // North east of Santos
+            );
+            const options = {
+              bounds: bounds,
+              strictBounds: true,
+              types: ['establishment'], // limita a pesquisa a estabelecimentos comerciais
+              componentRestrictions: {country: 'br'}, // limita a pesquisa ao Brasil
+            };
+            const input = document.getElementById('search-input');
+            const autocomplete = new google.maps.places.Autocomplete(input, options);
+            
+            map = new google.maps.Map(document.getElementById('map'), {
+              center: {lat: -23.9608, lng:-46.3331},
+              zoom: 12,
+              restriction: {
+                latLngBounds: {
+                  north: -23.8866,
+                  south: -24.0305,
+                  east: -46.2521,
+                  west: -46.4295
+                },
+                strictBounds: true
+              }
+            });	
+
+            function searchPlace() {
+                const input = document.getElementById('search-input');
+                const geocoder = new google.maps.Geocoder();
+                geocoder.geocode({ address: input.value }, (results, status) => {
+                  if (status === 'OK') {
+                    const location = results[0].geometry.location;
+                    map.setCenter(location);
+                    map.setZoom(15);
+                    const marker = new google.maps.Marker({
+                      position: location,
+                      map: map
+                    });
+                  } else {
+                    alert('Não foi possível encontrar o local.');
+                  }
+                });
+              }
+              document.getElementById('search-btn').addEventListener('click', searchPlace);
 
 			for(var i = 0; i < markers.length; i++){
 				var marker = new google.maps.Marker({
@@ -50,8 +92,8 @@ var map;
 				});
 				markers[i].marker = marker;
 			}
-           
-
+          
+              
 			document.getElementById('btn-skate').addEventListener('click', function() {
 				filterMarkers('skate');
 			});
@@ -59,6 +101,7 @@ var map;
 			document.getElementById('btn-basquete').addEventListener('click', function() {
 				filterMarkers('basquete');
 			});
+            
 		}
 
 		function filterMarkers(type) {
@@ -69,4 +112,5 @@ var map;
 					markers[i].marker.setVisible(false);
 				}
 			}
+            
 		}
