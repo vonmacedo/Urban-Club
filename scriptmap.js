b6eeb331735641afd51281e07b17d5f016269c97
vonmacedo <mjoaovictor388@gmail.com>
2023-05-10 02:28:12 -0300

    Merge branch 'main' of https://github.com/vonmacedo/Urban-Club

--- scriptmap.js
+++ scriptmap.js
@@ -1,24 +1,72 @@
-function initMap() {
-    const center = new google.maps.LatLng(-23.991249, -46.302834);
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15,
-        center: center,
-    });
-    const svgMarker = {
-        path: "M-1.547 12l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM0 0q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
-        fillColor: "blue",
-        fillOpacity: 0.6,
-        strokeWeight: 0,
-        rotation: 0,
-        scale: 2,
-        anchor: new google.maps.Point(0, 20),
-    };
+var map;
+		var markers = [
+			//skate
+			{
+				position: { lat: -23.9616758, lng:-46.3167502},
+				title: "Praça Palmares",
+				type: "skate"
+			},
+			{
+				position: {lat:-23.968807732192566, lng: -46.35150499213918 },
+				title: "Pista Quebra Mar",
+				type: "skate"
+			},
+			{
+				position: {lat:-23.947473172399416, lng:-46.3538606979173},
+				title: "Lagoa Skate Plaza",
+				type: "skate"
+			},
+			//basquete
+			{
+				position: {lat:-23.968618795130748, lng:-46.35038078657946},
+				title: "Quadra Quebra-Mar",
+				type: "basquete"
+			},
+			{
+				position: {lat:-23.978323795344334, lng:-46.30096832729573},
+				title: "Quadra Abor",
+				type: "basquete"
+			},
+			{
+				position: {lat:-23.93471937248423, lng:-46.321169032077776},
+				title: "Ginásio SDAS",
+				type: "basquete"
+			},
 
-    new google.maps.Marker({
-        position: map.getCenter(),
-        icon: svgMarker,
-        map: map,
-    });
-}
+		];
+		function initMap() {
+			map = new google.maps.Map(document.getElementById('map'), {
+				center: {lat: -23.9608, lng:-46.3331},
+				zoom: 14
+			});
 
-window.initMap = initMap;
+			for(var i = 0; i < markers.length; i++){
+				var marker = new google.maps.Marker({
+					position:markers[i].position,
+					title:markers[i].title,
+					map:map,
+					visible: true,
+					type: markers[i].type
+				});
+				markers[i].marker = marker;
+			}
+           
+
+			document.getElementById('btn-skate').addEventListener('click', function() {
+				filterMarkers('skate');
+			});
+
+			document.getElementById('btn-basquete').addEventListener('click', function() {
+				filterMarkers('basquete');
+			});
+		}
+
+		function filterMarkers(type) {
+			for(var i = 0; i < markers.length; i++){
+				if(markers[i].type == type){
+					markers[i].marker.setVisible(true);
+				} else {
+					markers[i].marker.setVisible(false);
+				}
+			}
+		}
