70506b27b54db12344fd1d7f1b0898466e266ce2
NomeJaEmUso <andreyvanolli@gmail.com>
2023-05-18 17:40:23 -0300

    Aside quase completa

--- scriptmap.js
+++ scriptmap.js
@@ -1,63 +1,61 @@
 var map;
-//skate
-var marker1 = {
-  position: { lat: -23.9616758, lng: -46.3167502 },
-  title: "Praça Palmares",
-  type: "skate",
-  photoUrl: "./img/praça-plamares.png"
-};
+var currentMarkerPosition;
+var currentRoute = null;
+var directionsService;
+var directionsRenderer;
 
-var marker2 = {
-  position: { lat: -23.968807732192566, lng: -46.35150499213918 },
-  title: "Pista Quebra Mar",
-  type: "skate",
-  photoUrl: "url_da_foto_skate.jpg"
-
-};
-
-var marker3 = {
-  position: { lat: -23.947473172399416, lng: -46.3538606979173 },
-  title: "Lagoa Skate Plaza",
-  type: "skate",
-  photoUrl: "url_da_foto_skate.jpg"
-};
-
-//basquete
-
-var marker4 = {
-  position: { lat: -23.968618795130748, lng: -46.35038078657946 },
-  title: "Quadra Quebra-Mar",
-  type: "basquete",
-  photoUrl: "url_da_foto_skate.jpg"
-};
-
-var marker5 = {
-  position: { lat: -23.978323795344334, lng: -46.30096832729573 },
-  title: "Quadra Abor",
-  type: "basquete",
-  photoUrl: "url_da_foto_skate.jpg"
-};
-
-var marker6 = {
-  position: { lat: -23.93471937248423, lng: -46.321169032077776 },
-  title: "Ginásio SDAS",
-  type: "basquete",
-  photoUrl: "url_da_foto_skate.jpg"
-};
-
-var markers = [marker1, marker2, marker3, marker4, marker5, marker6];
+var markers = [
+  {
+    position: { lat: -23.9616758, lng: -46.3167502 },
+    title: "Praça Palmares",
+    type: "skate",
+    photoUrl: "./img/praça-plamares.png"
+  },
+  {
+    position: { lat: -23.968807732192566, lng: -46.35150499213918 },
+    title: "Pista Quebra Mar",
+    type: "skate",
+    photoUrl: "url_da_foto_skate.jpg"
+  },
+  {
+    position: { lat: -23.947473172399416, lng: -46.3538606979173 },
+    title: "Lagoa Skate Plaza",
+    type: "skate",
+    photoUrl: "url_da_foto_skate.jpg"
+  },
+  {
+    position: { lat: -23.968618795130748, lng: -46.35038078657946 },
+    title: "Quadra Quebra-Mar",
+    type: "basquete",
+    photoUrl: "url_da_foto_skate.jpg"
+  },
+  {
+    position: { lat: -23.978323795344334, lng: -46.30096832729573 },
+    title: "Quadra Abor",
+    type: "basquete",
+    photoUrl: "url_da_foto_skate.jpg"
+  },
+  {
+    position: { lat: -23.93471937248423, lng: -46.321169032077776 },
+    title: "Ginásio SDAS",
+    type: "basquete",
+    photoUrl: "url_da_foto_skate.jpg"
+  }
+];
 
 function initMap() {
   const bounds = new google.maps.LatLngBounds(
     new google.maps.LatLng(-23.98604, -46.396624), // South west of Santos
     new google.maps.LatLng(-23.928531, -46.266352) // North east of Santos
   );
+
   const options = {
     bounds: bounds,
     strictBounds: true,
     types: ['establishment'], // limita a pesquisa a estabelecimentos comerciais
     componentRestrictions: { country: 'br' } // limita a pesquisa ao Brasil
   };
+
   const input = document.getElementById('search-input');
   const autocomplete = new google.maps.places.Autocomplete(input, options);
 
@@ -89,6 +87,7 @@ function initMap() {
       }
     });
   }
+
   document.getElementById('search-btn').addEventListener('click', searchPlace);
 
   for (var i = 0; i < markers.length; i++) {
@@ -100,23 +99,28 @@ function initMap() {
       type: markers[i].type,
       photoUrl: markers[i].photoUrl
     });
-    markers[i].marker = marker;
-  
+
     marker.addListener('click', function() {
       var aside = document.getElementById('aside');
       aside.classList.remove('hidden');
+
+      document.getElementById('localizacao').textContent = this.position;
+      document.getElementById('aberto-24h').textContent = ""; // Preencher com a informação correta
+      document.getElementById('tipo-local').textContent = this.type;
 
       var markerName = document.getElementById('marker-name');
       var markerPhoto = document.getElementById('marker-photo');
       markerName.textContent = this.getTitle();
       markerPhoto.src = this.photoUrl;
-      
+
+      currentMarkerPosition = this.getPosition();
     });
   }
+
   map.addListener('click', function(event) {
     var aside = document.getElementById('aside');
     var asideContent = document.getElementById('aside-content');
-  
+
     // Verifica se o clique ocorreu fora do aside
     if (!aside.contains(event.target) && asideContent !== event.target) {
       aside.classList.add('hidden'); // Adiciona a classe 'hidden' para esconder o aside
@@ -130,6 +134,18 @@ function initMap() {
   document.getElementById('btn-basquete').addEventListener('click', function() {
     filterMarkers('basquete');
   });
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    userPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+    const userMarker = new google.maps.Marker({
+      position: userPosition,
+      map: map,
+      title: 'Sua localização'
+    });
+  }, function() {
+    window.alert('Não foi possível obter a localização do usuário.');
+  });
 }
 
 function filterMarkers(type) {
@@ -141,3 +157,44 @@ function filterMarkers(type) {
     }
   }
 }
+
+function createRoute(destination) {
+  if (currentRoute) {
+    currentRoute.setMap(null); // Remove a rota atual do mapa
+    currentRoute = null; // Define a rota atual como null
+  }
+
+  directionsService = new google.maps.DirectionsService();
+  directionsRenderer = new google.maps.DirectionsRenderer({ map: map });
+
+  var routeOptions = {
+    origin: userPosition,
+    destination: destination,
+    travelMode: 'DRIVING' // Modo de transporte (DRIVING, WALKING, etc.)
+  };
+
+  directionsService.route(routeOptions, function(response, status) {
+    if (status === 'OK') {
+      directionsRenderer.setDirections(response);
+      currentRoute = directionsRenderer;
+    } else {
+      window.alert('Falha ao calcular a rota: ' + status);
+    }
+  });
+}
+
+var btnRota = document.getElementById('btn-rota');
+
+// Adiciona o evento de clique ao botão
+btnRota.addEventListener('click', function() {
+  if (currentMarkerPosition) {
+    createRoute(currentMarkerPosition);
+  }
+});
+
+// Obtém a localização do usuário
+navigator.geolocation.getCurrentPosition(function(position) {
+  userPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+}, function() {
+  window.alert('Não foi possível obter a localização do usuário.');
+});
